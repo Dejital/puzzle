d9e5610f893a14d56c756c930fee3d10f61396d6
Serge Nevsky <sergey@sergeyk.com>
2016-06-12 03:25:46 -0500

    Piece placement better defined.

--- public/puzzle.js
+++ public/puzzle.js
@@ -6,27 +6,46 @@
 
   var board = document.querySelector('.game-board');
   var pieces = document.querySelector('.game-pieces');
+  
+  var colors = ['blue', 'magenta', 'lawngreen', 'goldenrod', 'cyan'];
 
-  this.setupBoard = function(board, dimension) {
+  var defaultBoardConfiguration = [4,2,4,1,3,1,3,0,2,0,2,1,1,4,3,0];
+  var defaultPiecesDistribution = [3,3,3,4,3];
+
+  this.setupBoard = function(board, configuration, dimension) {
 
     var game = this;
     board.innerHTML = '';
+    var id = 0;
 
     for (var x = 0; x < dimension; x++) {
       for (var y = 0; y < dimension; y++) {
+
+        var type = configuration[id];
+        var color = colors[type];
+
         spot = document.createElement('span');
         spot.className = 'spot';
         spot.setAttribute('x', x);
         spot.setAttribute('y', y);
         spot.setAttribute('isOpen', true);
+        spot.setAttribute('type', type);
+        spot.setAttribute('style', 'background:' + color);
 
         spot.onclick = function () {
-          if (game.selectedPiece){
+          var isOpen = this.getAttribute('isOpen');
+          debugger;
+          if (isOpen === 'true' && game.selectedPiece){
+
             // TODO: Check if legal to place
 
             game.selectedPiece.className = 'piece-hidden';
             game.selectedPiece.setAttribute('isPlaced', true);
 
+            var type = game.selectedPiece.getAttribute('type');
+            var color = colors[type];
+            this.setAttribute('isOpen', false);
+            this.setAttribute('style', 'background:' + color);
             this.className += ' spot-on';
 
             game.selectedPiece = '';
@@ -40,6 +59,7 @@
 
         this.board.push(spot);
         board.appendChild(spot);
+        id++;
       }
     }
 
@@ -57,31 +77,37 @@
     }
   };
 
-  this.setupPieces = function(pieces, count) {
+  this.setupPieces = function(pieces, distribution) {
 
     var game = this;
     pieces.innerHTML = '';
 
-    for (var i = 0; i < count; i++) {
-      piece = document.createElement('span');
-      piece.className = 'piece';
-      piece.setAttribute('isPlaced', false);
+    for (var i = 0; i < distribution.length; i++) {
+      while (distribution[i] > 0){
 
-      piece.onclick = function () {
-        game.resetPieces();
-        this.className = 'piece piece-selected';
-        game.selectedPiece = this;
-      };
+        piece = document.createElement('span');
+        piece.className = 'piece';
+        piece.setAttribute('isPlaced', false);
+        piece.setAttribute('type', i);
+        piece.setAttribute('style', 'background:' + colors[i]);
 
-      this.pieces.push(piece);
-      pieces.appendChild(piece);
+        piece.onclick = function () {
+          game.resetPieces();
+          this.className = 'piece piece-selected';
+          game.selectedPiece = this;
+        };
+
+        this.pieces.push(piece);
+        pieces.appendChild(piece);
+        distribution[i]--;
+      }
     }
 
   };
 
-  var dimension = 5;
+  var dimension = 4;
 
-  this.setupBoard(board, dimension);
-  this.setupPieces(pieces, dimension * dimension);
+  this.setupBoard(board, defaultBoardConfiguration, dimension);
+  this.setupPieces(pieces, defaultPiecesDistribution);
 
 })();
