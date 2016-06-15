736619fcf321d4089c04da13593434116c6d6ab9
Serge Nevsky <sergey@sergeyk.com>
2016-06-14 21:59:16 -0500

    Use strict mode

--- public/puzzle.js
+++ public/puzzle.js
@@ -1,23 +1,25 @@
-(function() {
+'use strict';
 
-  this.board = [];
-  this.pieces = [];
-  this.selectedPiece = '';
-  this.score = 0;
+function puzzle() {
 
-  var board = document.querySelector('.game-board');
-  var pieces = document.querySelector('.game-pieces');
-  var counter = document.querySelector('.counter');
+  var board = [];
+  var pieces = [];
+  var selectedPiece = '';
+  var score = 0;
+
+  var boardContainer = document.querySelector('.game-board');
+  var piecesContainer = document.querySelector('.game-pieces');
+  var counterContainer = document.querySelector('.counter');
 
   var colors = ['blue', 'magenta', 'lawngreen', 'goldenrod', 'cyan'];
 
   var defaultBoardConfiguration = [4,2,4,1,3,1,3,0,2,0,2,1,1,4,3,0];
   var defaultPiecesDistribution = [3,3,3,4,3];
+  var dimension = 4;
 
-  this.setupBoard = function(board, configuration, dimension) {
+  function setupBoard(configuration, dimension) {
 
-    var game = this;
-    board.innerHTML = '';
+    boardContainer.innerHTML = '';
     var id = 0;
 
     for (var y = 0; y < dimension; y++) {
@@ -27,7 +29,7 @@
         var type = configuration[id];
         var color = colors[type];
 
-        spot = document.createElement('span');
+        var spot = document.createElement('span');
         spot.className = 'spot';
         spot.setAttribute('x', x);
         spot.setAttribute('y', y);
@@ -39,11 +41,7 @@
           var isOpen = this.getAttribute('isOpen');
           var x = parseInt(this.getAttribute('x'));
           var y = parseInt(this.getAttribute('y'));
-          if (game.canPlacePiece(x,y)){
-
-            // TODO: Check if legal to place
-
-            var selectedPiece = game.selectedPiece;
+          if (canPlacePiece(x,y)){
             selectedPiece.className = 'piece-hidden';
             selectedPiece.setAttribute('isPlaced', true);
 
@@ -54,10 +52,10 @@
             this.setAttribute('style', 'background:' + color);
             this.className += ' spot-on';
 
-            game.selectedPiece = '';
-            game.resetPieces();
-            game.score++;
-            game.updateCounter(game.score);
+            selectedPiece = '';
+            resetPieces();
+            score++;
+            updateCounter(score);
           }
         };
 
@@ -66,97 +64,92 @@
         }
 
         row.push(spot);
-        board.appendChild(spot);
+        boardContainer.appendChild(spot);
         id++;
       }
-      this.board.push(row);
+      board.push(row);
     }
 
-  };
+  }
 
-  this.resetPieces = function() {
-    var game = this;
-
-    for (var i = 0; i < game.pieces.length; i++){
-      var piece = game.pieces[i];
+  function resetPieces() {
+    for (var i = 0; i < pieces.length; i++){
+      var piece = pieces[i];
       var isPlaced = piece.getAttribute('isPlaced');
       if (isPlaced === 'false'){
         piece.className = 'piece';
       }
     }
-  };
+  }
 
-  this.setupPieces = function(pieces, distribution) {
+  function setupPieces(distribution) {
 
-    var game = this;
-    pieces.innerHTML = '';
+    piecesContainer.innerHTML = '';
 
     for (var i = 0; i < distribution.length; i++) {
       while (distribution[i] > 0){
 
-        piece = document.createElement('span');
+        var piece = document.createElement('span');
         piece.className = 'piece';
         piece.setAttribute('isPlaced', false);
         piece.setAttribute('type', i);
         piece.setAttribute('style', 'background:' + colors[i]);
 
         piece.onclick = function () {
-          game.resetPieces();
+          resetPieces();
           this.className = 'piece piece-selected';
-          game.selectedPiece = this;
+          selectedPiece = this;
         };
 
-        this.pieces.push(piece);
-        pieces.appendChild(piece);
+        pieces.push(piece);
+        piecesContainer.appendChild(piece);
         distribution[i]--;
       }
     }
 
-  };
+  }
 
-  this.canPlacePiece = function(x, y) {
-    if (!this.selectedPiece)
+  function canPlacePiece(x, y) {
+    if (!selectedPiece)
       return false;
 
-    var spot = this.board[y][x];
+    var spot = board[y][x];
     if (spot.getAttribute('isOpen') === 'false')
       return false;
 
-    var type = this.selectedPiece.getAttribute('type');
-    var length = this.board.length;
+    var type = selectedPiece.getAttribute('type');
+    var length = board.length;
     if (spot.getAttribute('type') === type)
       return false;
-    if (y > 0 && this.board[y-1][x].getAttribute('type') === type)
+    if (y > 0 && board[y-1][x].getAttribute('type') === type)
       return false;
-    if (y < length-1 && this.board[y+1][x].getAttribute('type') === type)
+    if (y < length-1 && board[y+1][x].getAttribute('type') === type)
       return false;
-    if (x > 0 && this.board[y][x-1].getAttribute('type') === type)
+    if (x > 0 && board[y][x-1].getAttribute('type') === type)
       return false;
-    if (x < length-1 && this.board[y][x+1].getAttribute('type') === type)
+    if (x < length-1 && board[y][x+1].getAttribute('type') === type)
       return false;
-    if (x > 0 && y > 0 && this.board[y-1][x-1].getAttribute('type') === type)
+    if (x > 0 && y > 0 && board[y-1][x-1].getAttribute('type') === type)
       return false;
-    if (x < length-1 && y > 0 && this.board[y-1][x+1].getAttribute('type') === type)
+    if (x < length-1 && y > 0 && board[y-1][x+1].getAttribute('type') === type)
       return false;
-    if (x > 0 && y < length-1 && this.board[y+1][x-1].getAttribute('type') === type)
+    if (x > 0 && y < length-1 && board[y+1][x-1].getAttribute('type') === type)
       return false;
-    if (x < length-1 && y < length-1 && this.board[y+1][x+1].getAttribute('type') === type)
+    if (x < length-1 && y < length-1 && board[y+1][x+1].getAttribute('type') === type)
       return false;
 
     return true;
-  };
+  }
 
-  this.updateCounter = function(score) {
+  function updateCounter(score) {
     if (score === dimension * dimension)
-      counter.innerHTML = score + ' Success!';
+      counterContainer.innerHTML = score + ' Success!';
     else
-      counter.innerHTML = score;
-  };
+      counterContainer.innerHTML = score;
+  }
 
-  var dimension = 4;
+  setupBoard(defaultBoardConfiguration, dimension);
+  setupPieces(defaultPiecesDistribution);
+  updateCounter(0);
 
-  this.setupBoard(board, defaultBoardConfiguration, dimension);
-  this.setupPieces(pieces, defaultPiecesDistribution);
-  this.updateCounter(0);
-
-})();
+}
